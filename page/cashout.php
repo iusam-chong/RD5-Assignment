a8cc8b981f2009f187377954dd6a3fe5f231f9bc
sam_chong <sam_chong@mail.chungyo.net>
2020-08-28 17:00:40 +0800

    add front-view page

---
+++ page/cashout.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>首頁</title>
+
+<!-- Custom fonts for this template-->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gray-300">
+<div class="container">
+<div class="card o-hidden border-0 shaow-lg my-5">
+<div class="card-body p-0">
+  <!-- main content -->
+  <div class="row">
+    <div class="col-lg-3">
+        
+      <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item"><a href="#" class="">首頁</a></li>
+            <li class="list-group-item"><a href="#" class="">存款</a></li>
+            <li class="list-group-item"><a href="#" class="">提款</a></li>
+            <li class="list-group-item"><a href="#" class="">查看明細</a></li>
+            <li class="list-group-item"><a href="#" class="">會員資料</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col-lg-9">
+      <div class="card-body">
+
+        <div class="text-center">
+            <h1 class="h4 text-gray-900 mb-4">提款</h1>
+        </div>
+
+        <form class="user">
+            <div class="form-group">
+                <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="輸入金額">
+            </div>
+            
+            <a href="index.html" class="btn btn-primary btn-user btn-block">
+                確認
+            </a>
+        </form>
+       
+      </div>
+    </div>
+
+  </div>
+  <!-- main content end -->
+</div>
+</div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="js/demo/chart-area-demo.js"></script>
+<script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
